5a4678ceac90d56838a54cd7e8a8e1b79c912af1
Z <zijianhuang2000@gmail.com>
2018-11-16 20:08:26 +1000

    ios example

---
+++ Mobile/Fonlow.Heroes.iOS/AppDelegate.cs
@@ -0,0 +1,32 @@
+﻿using Foundation;
+using UIKit;
+using Xamarin.Forms.Platform.iOS;
+using Fonlow.Heroes.VM;
+using Fonlow.Heroes.Views;
+
+namespace Fonlow.Heroes.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    [Register("AppDelegate")]
+    public class AppDelegate  : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        // class-level declarations
+
+        public override UIWindow Window
+        {
+            get;
+            set;
+        }
+
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            global::Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
+
+            return base.FinishedLaunching(application, launchOptions);
+        }
+
+    }
+}
+
