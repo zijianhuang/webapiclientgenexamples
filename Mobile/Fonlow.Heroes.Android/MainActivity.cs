184198f0db1cd4b211faac114b2ebce3f2ec1622
zijianhuang <zijianhuang2000@gmail.com>
2023-11-15 13:57:07 +1000

    update

--- Mobile/Fonlow.Heroes.Android/MainActivity.cs
+++ Mobile/Fonlow.Heroes.Android/MainActivity.cs
@@ -1,42 +1,43 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using Android.Views;
 
 
 namespace Fonlow.Heroes
 {
-    [Activity(Label = "FonlowAndroid", Icon = "@mipmap/icon", 
-        Theme = "@style/MainTheme", 
-        MainLauncher = true, 
-        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
-        HardwareAccelerated =true
-        )]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	[Activity(Label = "Heroes", Icon = "@mipmap/icon", 
+		Theme = "@style/MainTheme", 
+		MainLauncher = true, 
+		ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
+		HardwareAccelerated =true
+		)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
-        protected override void OnCreate(Bundle bundle)
-        {
-            //TabLayoutResource = Resource.Layout.Tabbar;
-            //ToolbarResource = Resource.Layout.Toolbar;
-            
-            base.OnCreate(bundle);
+		protected override void OnCreate(Bundle bundle)
+		{
+			//Both lines needed, otherwise in IDE Debug with x86 emulator base.OnCreate will throw System.InvalidCastException
+			//Message = Unable to convert instance of type 'Android.Widget.RelativeLayout' to type 'AndroidX.AppCompat.Widget.Toolbar'.	
+			//And the APK deployment will crash, though aab deployment is OK.
+			ToolbarResource = Resource.Layout.Toolbar;
+			TabLayoutResource = Resource.Layout.Tabbar;
 
-            global::Xamarin.Forms.Forms.Init(this, bundle);
+			base.OnCreate(bundle);
 
-            var app = new App();
-            LoadApplication(app);
-        }
+			global::Xamarin.Forms.Forms.Init(this, bundle);
+		}
 
-        public override void SetContentView(View view)
-        {
-            base.SetContentView(view);
-        }
+		protected override void OnPostCreate(Bundle savedInstanceState)
+		{
 
+			global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+			//global::Xamarin.Forms.FormsMaterial.Init(this, savedInstanceState);
+			var app = new App();//Settings is read
+			LoadApplication(app);
 
-    }
+			//Plugin.StoreReview.CrossStoreReview.Current.OpenStoreReviewPage("com.fonlow.VA");
+			base.OnPostCreate(savedInstanceState);
+		}
+	}
 }
 
